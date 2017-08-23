817712b4007c3493f26d75205a31774e08815a9e
Hector <xuguang.r@gmail.com>
2017-08-23 10:14:44 +1000

    update build.gradle

--- app/src/main/java/com/griffith/autoflow/fragment/HomeFragment.java
+++ app/src/main/java/com/griffith/autoflow/fragment/HomeFragment.java
@@ -41,7 +41,7 @@ public class HomeFragment extends Fragment {
 
     private List<Float> usageVolumes;
 
-    private LoadUsageDataTask mLoadUsageDataTask = null;
+    //private LoadUsageDataTask mLoadUsageDataTask = null;
 
 
     private String mUsername;
@@ -69,115 +69,10 @@ public class HomeFragment extends Fragment {
                 Context.MODE_PRIVATE);
         mUsername = sharedPref.getString(Constant.SHARED_PREFERENCE_USERNAME, "");
 
-        mLoadUsageDataTask = new LoadUsageDataTask();
-        mLoadUsageDataTask.execute();
+        /*mLoadUsageDataTask = new LoadUsageDataTask();
+        mLoadUsageDataTask.execute();*/
 
         return view;
     }
-
-    private class LoadUsageDataTask extends AsyncTask<Void, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(activity);
-            //mProgressDialog.setTitle("Login");
-            mProgressDialog.setMessage("Logging in...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-
-            String urlString = "http://10.0.0.9:8000/personal/api/usage?username=";
-            urlString += mUsername;
-            InputStream in = null;
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                // get response
-                in = new BufferedInputStream(conn.getInputStream());
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-            try {
-                String responseJsonString = IOUtils.toString(in, "UTF-8");
-                return responseJsonString;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return "";
-        }
-
-        @Override
-        protected void onPostExecute(final String responseJsonString) {
-            mProgressDialog.dismiss();
-            mLoadUsageDataTask = null;
-
-            try {
-                JSONObject jsonObject = new JSONObject(responseJsonString);
-                JSONArray volArray = jsonObject.getJSONArray("vol");
-                usageVolumes = new ArrayList<Float>();
-                for (int i = 0; i < volArray.length(); i++) {
-                    double vol = volArray.getDouble(i);
-                    usageVolumes.add((float)vol);
-                }
-                drawUsageChart();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mProgressDialog.dismiss();
-            mLoadUsageDataTask = null;
-            //showProgress(false);
-        }
-    }
-
-    private void drawUsageChart() {
-        float total = usageVolumes.get(0)
-                + usageVolumes.get(1)
-                + usageVolumes.get(2)
-                + usageVolumes.get(3)
-                + usageVolumes.get(4)
-                + usageVolumes.get(5)
-                + usageVolumes.get(6)
-                + usageVolumes.get(7)
-                + usageVolumes.get(8)
-                + usageVolumes.get(9);
-
-
-        ArrayList<PieEntry> usageEntries = new ArrayList<>();
-        usageEntries.add(new PieEntry(usageVolumes.get(0) * 100/total, "Shower", 0));
-        usageEntries.add(new PieEntry(usageVolumes.get(1) * 100/total, "Tap", 1));
-        usageEntries.add(new PieEntry(usageVolumes.get(2)* 100/total, "Clothes washer", 2));
-        usageEntries.add(new PieEntry(usageVolumes.get(3) * 100/total, "Dishwasher", 3));
-        usageEntries.add(new PieEntry(usageVolumes.get(4) * 100/total, "Toilet", 4));
-        usageEntries.add(new PieEntry(usageVolumes.get(5)* 100/total, "Bathtub", 5));
-        usageEntries.add(new PieEntry(usageVolumes.get(6) * 100/total, "Irrigation", 6));
-        usageEntries.add(new PieEntry(usageVolumes.get(7) * 100/total, "Evap cooler", 7));
-        usageEntries.add(new PieEntry(usageVolumes.get(8)* 100/total, "Leak", 8));
-        usageEntries.add(new PieEntry(usageVolumes.get(9)* 100/total, "Leak", 9));
-
-        PieDataSet usageDataset = new PieDataSet(usageEntries, "# of Calls");
-
-        PieData usageData = new PieData();
-        usageData.setDataSet(usageDataset);
-        usageDataset.setColors(ColorTemplate.COLORFUL_COLORS); //
-
-        usagePieChart.setCenterText("usage Level");
-        usagePieChart.setData(usageData);
-        usagePieChart.getDescription().setEnabled(false);
-        usagePieChart.setDrawSliceText(false);
-        usagePieChart.animateY(2000);
-
-        usagePieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
-
-    }
-
 
 }
